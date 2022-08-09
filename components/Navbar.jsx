98f77f00dfc19f489e90837ead81bf8277fed7e0
shahin <shahin.shahi1986@gmail.com>
2022-08-09 14:46:25 +0300

    make navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import { Menu, MenuButton, MenuList, MenuItem, IconButton, Flex, Box, Spacer } from '@chakra-ui/react';
+import { FcMenu, FcHome, FcAbout } from 'react-icons/fc';
+import { BsSearch } from 'react-icons/bs';
+import { FiKey } from 'react-icons/fi';
+
+const Navbar = () => (
+
+    <Flex p='2' borderBottom='1px' borderColor='gray.100'>
+        <Box fontSize='3xl' color='blue.400' fontWeight='bold'>
+            <Link href='/' paddingLeft='2'>Realtor</Link>
+        </Box>
+    </Flex>
+
+)
+export default Navbar
