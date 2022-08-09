f48543a183122601006c3f2b99b7355b2f1d02d5
shahin <shahin.shahi1986@gmail.com>
2022-08-09 12:41:04 +0300

    make assets component and call image form rapidapi

--- components/Property.jsx
+++ components/Property.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Link from "next/link"
+import Image from "next/image"
+import { Box, Flex, Avatar, Text } from '@chakra-ui/react'
+import { FaBed, FaBath } from 'react-icons/fa'
+import { BsGridFill } from 'react-icons/bs'
+import { GoVerified } from 'react-icons/go'
+import millify from 'millify'
+import DefaultImage from '../assets/images/house.jpg'
 
-const Property = () => {
-  return (
-    <div>Property</div>
-  )
-}
-
+const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalId } }) => (
+    <Link href={`/property/${externalId}`} passHref>
+        <Flex flexWrap="wrap" w="420px" p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer">
+            <Box>
+                <Image src={coverPhoto ? coverPhoto.url : DefaultImage} alt="house" width={400} height={260} />
+            </Box>
+        </Flex>
+    </Link>
+)
 export default Property
